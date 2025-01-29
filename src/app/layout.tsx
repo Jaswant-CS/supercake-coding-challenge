a6773cd387d96e9d74f7cdf474a17e59f9e72561
Jaswant Saini <jaswantsaini@cssoftsolutions.com>
2025-01-29 16:43:43 +0530

    Favicon Added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/public/supercake-transparent.webp" />
+        <link rel="icon" href="/supercake-transparent.webp" sizes="any" />
+      </head>
       <body suppressHydrationWarning={true}>{children}</body>
     </html>
   );
